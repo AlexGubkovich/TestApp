5f772c562f00a53ad632a9f52e7449b46bd1c67a
AlexGubkovich <alex.gubkovich@gmail.com>
2023-07-22 09:43:04 +0300

    finished companies page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,14 @@ namespace TestApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Details()
+        public IActionResult Details([FromRoute] int id)
         {
-            return View();
+            return Ok(id);
         }
 
         public IActionResult Privacy()
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,17 +1,6 @@
-﻿const uri = 'api/companies';
-
-function getCompanies() {
+﻿function getRequest(uri, handleData) {
     fetch(uri)
         .then(respose => respose.json())
-        .then(data => displayCompanies(data))
-        .catch(error => console.error('Unable to get items.', error));
+        .then(data => handleData(data))
+        .catch(error => console.error(error));
 }
-
-function displayCompanies(companies) {
-    $.each(companies, function (i, company) {
-        $('#companiesTable > tbody:last-child')
-            .append(`<tr><td>${company.name}</td><td>${company.city}</td><td>${company.state}</td><td>${company.phone}</td></tr>`)
-    })
-}
-
-window.onload = getCompanies();
